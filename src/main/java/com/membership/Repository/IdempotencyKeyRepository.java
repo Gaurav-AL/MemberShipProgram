769d4c6614af673a4e376e510eed099dbd62f3c1
Gaurav-AL <reverse454@gmail.com>
2026-01-10 11:36:27 +0530

    1st commit

--- src/main/java/com/membership/Repository/IdempotencyKeyRepository.java
+++ src/main/java/com/membership/Repository/IdempotencyKeyRepository.java
@@ -4,11 +4,15 @@ import java.util.UUID;
 
 import org.hibernate.boot.beanvalidation.GroupsPerOperation.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.membership.dao.IdempotencyKeys;
 import com.membership.dao.Operations;
 import java.util.Optional;
 
 
+
+@Repository
 public interface IdempotencyKeyRepository extends JpaRepository<IdempotencyKeys, UUID>{
     Optional<IdempotencyKeys> findByRequestHash(String requestHash);
 
